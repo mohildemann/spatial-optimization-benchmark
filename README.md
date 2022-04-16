d6102a3f290822ee64e82474c27c139a33da0e07
Jan Munstermann <janmunstermann1995@gmail.com>
2022-04-16 11:03:30 -0700

    adjusted app.py and filled readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # spatial-optimization-benchmark
+This project invokes a webside, that will be used to give a spatial overview for a optimization problem with regards to a precalculated pareto front.
--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
+import pandas as pd
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 import plotly.io as pio
